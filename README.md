014cf3153ba880edfc3d48702d570cfb7055b5cb
Ansel <aesthapus@gmail.com>
2014-08-07 16:26:34 -0600

    added script to install teamviewer via gdebi

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-handy-scripts
-=============
+###Handy Scripts
---
+++ install_teamviewer.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+sudo apt-get install gdebi
+wget http://www.teamviewer.com/download/teamviewer_linux.deb
+sudo gdebi teamviewer_linux.deb
