c08ffda2925536cdd510a27a7e30c277f989aaf5
Ansel <aesthapus@gmail.com>
2015-03-02 13:29:03 -0500

    added graphics card info to plot title in plotPerformance.r

--- plotPerformance.r
+++ plotPerformance.r
@@ -3,12 +3,16 @@
 require("ggplot2")
 require("reshape2")
 
-filebit = "xenomai-sparrow-becca-50kHz"
+# Headings for the plot. Edit them here. 
+filebit = "penguin-xenomai-radeon-50kHz" #used to specify input/output filenames
 rt_patch = "Xenomai 2.6.3"
-processor = ""
+processor = "i5-4570"
+graphics_card = "AMD HD 8490"
+graphics_driver = "Radeon"
 os = "Debian 8"
 rate = 50 #kHz
 
+# Read the data and check if it has three variables. 
 data = read.table(paste(filebit, ".txt", sep=""), sep=',', header=F)
 if (ncol(data) != 3) {
 	print("Check the number of variables.\n\t(Hint: the three you need are Computation Time, Real-time Period, and Jitter)\n ...Exiting\n")
@@ -17,7 +21,7 @@ if (ncol(data) != 3) {
 	colnames(data)  = c("CompTime", "Period", "Jitter")
 }
 
-# Scale the data
+# Scale the data.
 data$Time = seq(1, length(data[,1]))/(rate*1000) # to s
 data$CompTime  = data$CompTime / 1000 # to us
 data$Period = data$Period / 1000 # to us
@@ -25,11 +29,12 @@ data$Jitter = data$Jitter / 1000 # to us
 
 data.m = melt(data, id.vars='Time')
 
+# Create the plot. 
 perfplot = ggplot(data.m, aes(x=Time, y=value, colour=variable)) + 
-	geom_point(shape=16,alpha=.10) + 
+	geom_point(shape=16,alpha=.1) + 
 	facet_wrap( ~ variable, scales="free", ncol=1) + 
 	labs(x="Time (s)", y=expression(paste("Time (", mu, "s)"))) + 
 	guides(colour=FALSE) +
-	ggtitle(paste(os, rt_patch, processor, "\n", "Recording at", rate, "kHz", sep=" "))
+	ggtitle(paste(os, rt_patch, "\n", processor, graphics_card, graphics_driver, "\n", "Recording at", rate, "kHz", sep=" "))
 
 ggsave(paste(filebit, ".png", sep=""))
