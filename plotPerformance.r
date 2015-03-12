da6739917b05f0ec03f62bc5102426fd00151ed1
anselg <aesthapus@gmail.com>
2015-03-12 17:51:56 -0400

    added system info table to plot. Plot is HUUUGE

--- plotPerformance.r
+++ plotPerformance.r
@@ -3,16 +3,18 @@
 options(warn=-1)
 args = commandArgs(trailingOnly=T)
 
-if( !(require(ggplot2)&&require(reshape2)) ) {
-   message("I need some R packages installed. (ggplot2, plyr, gridExtra, and scales)")
+if( !(require(ggplot2)&&require(reshape2)&&require(gridExtra)) ) {
+   message("I need some R packages installed. (ggplot2 and gridExtra)")
    message(prompt="Want me to install them? (y/N)")
    val <- scan("stdin", character(), n=1)
    if ((val == "y")||(val == "Y")) {
       install.packages("ggplot2", repos="http://watson.nci.nih.gov/cran_mirror/")
       install.packages("reshape2", repos="http://watson.nci.nih.gov/cran_mirror/")
+      install.packages("gridExtra", repos="http://watson.nci.nih.gov/cran_mirror/")
 
       require(ggplot2)
       require(reshape2)
+		require(gridExtra)
    } else {
       stop("Look, you need to install 'ggplot2', 'plyr', 'gridExtra', and 'scales' to do this.")
    }
@@ -70,9 +72,22 @@ data.stats$CompTime  = data.stats$CompTime / 1000 # to us
 data.stats$Period = data.stats$Period / 1000 # to us
 data.stats$Jitter = data.stats$Jitter / 1000 # to us
 
+# System info passed from command line
+data.system = data.frame(Field = c("Operating System", "Host Name", "RT Kernel", "Processor", "Graphics Card", "Graphics Driver"), Info = c(os, hostname, rt_kernel, processor, graphics_card, graphics_driver) )
+
 data.m = melt(data.stats, id.vars='Time')
 
-# Create the plot. 
+plot.system = qplot(1:10, 1:10, geom = "blank") +
+   theme_bw() +
+   theme(line = element_blank(), text = element_blank(), panel.grid.major = element_blank(),
+         panel.grid.minor = element_blank(), panel.border = element_blank(),
+         panel.margin = element_blank() ) +
+   annotation_custom(grob = tableGrob(data.system, core.just="left", show.colnames=F,
+                                      row.just = "left", col.just = "left",
+                                      padding.h = unit(5,"mm"),
+                                      gpar.coretext = gpar(cex=1)),
+                     xmin=1, xmax=10, ymin=1, ymax=10)
+
 perfplot = ggplot(data.m, aes(x=Time, y=value, colour=variable)) + 
 	geom_point(shape=16,alpha=.1) + 
 	facet_wrap( ~ variable, scales="free", ncol=1) + 
@@ -80,4 +95,11 @@ perfplot = ggplot(data.m, aes(x=Time, y=value, colour=variable)) +
 	guides(colour=FALSE) #+
 #	ggtitle(paste(os, rt_patch, "\n", processor, graphics_card, graphics_driver, "\n", "Recording at", rate, "kHz", sep=" "))
 
-ggsave(filename=outfile, plot=perfplot)
+#ggsave(filename=outfile, plot=perfplot)
+svg(outfile, width=9, height=12 )  # File is HUUUGE
+grid.newpage()
+pushViewport(viewport(layout = grid.layout(4,1), width=1, height=1))
+print(plot.system, vp = viewport(layout.pos.row = 1, layout.pos.col = 1))
+print(perfplot, vp = viewport(layout.pos.row = 2:4, layout.pos.col = 1))
+dev.off()
+
