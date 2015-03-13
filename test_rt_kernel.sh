4c7ee52d7620ac369f6e8b0a409d58e6224db871
Ansel <aesthapus@gmail.com>
2015-03-13 18:20:04 -0400

    finalized kernel test script for push to qt4 branch

--- test_rt_kernel.sh
+++ test_rt_kernel.sh
@@ -1,4 +1,3 @@
-#
 # The Real-Time eXperiment Interface (RTXI)
 # Copyright (C) 2011 Georgia Institute of Technology, University of Utah, Weill Cornell Medical College
 #
@@ -16,16 +15,11 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 # Created by Yogi Patel <yapatel@gatech.edu> 2014.1.31
-#
 
 #!/bin/bash
 
 # Check to see if R and stress are installed
 echo "----->Checking for dependencies needed to run stress test."
-#if ! $(dpkg-query -Wf'${db:Status-abbrev}' "inxi" 2>/dev/null | grep -q '^i'); 
-#	then sudo apt-get -y install inxi
-#fi
-
 if ! $(dpkg-query -Wf'${db:Status-abbrev}' "stress" 2>/dev/null | grep -q '^i'); 
 	then sudo apt-get -y install stress
 fi
@@ -44,15 +38,11 @@ echo "      $ pkill stress"
 echo ""
 
 # Get system information
-#DISTRO_NAME=`lsb_release -is`
-#DISTRO_VERSION=`lsb_release -rs`
 DISTRO="$(lsb_release -is) $(lsb_release -rs)"
 HOSTNAME=`uname -n`
 RT_KERNEL=`uname -r`
-PROCESSOR=$(cat /proc/cpuinfo | grep "model name" | uniq | cut -d":" -f2 | \
-            sed 's/ \+/ /g' | sed -e 's/^\  *//' -e 's/\ *$//')
-GRAPHICS_CARD=$(lspci | grep VGA | uniq | cut -d":" -f3 | \
-                sed 's/ \+/ /g' | sed -e 's/^\  *//' -e 's/\ *$//')
+PROCESSOR=$(cat /proc/cpuinfo | grep "model name" | uniq | cut -d":" -f2 | sed 's/ \+/ /g' | sed -e 's/^\  *//' -e 's/\ *$//')
+GRAPHICS_CARD=$(lspci | grep VGA | uniq | cut -d":" -f3 | sed 's/ \+/ /g' | sed -e 's/^\  *//' -e 's/\ *$//')
 GRAPHICS_DRIVER=$(lshw -c display | grep "configuration: driver" | cut -d":" -f2 | cut -d"=" -f2 | cut -d" " -f1 | sed 's/ \+/ /g' | sed -e 's/^\  *//' -e 's/\ *$//')
 
 # Set up variables for run
@@ -60,16 +50,10 @@ TIME=1800 # duration of run (s)
 RT_PERIOD=100
 RATE=$(expr 1000 / $RT_PERIOD) # Convert RT period to freq in kHz
 
-#sudo bash -c "echo 0 > /proc/xenomai/latency" # necessary for histogram to work
-
 # Run latency test under dynamic load
 stress --cpu 2 --vm 1 --hdd 1 --timeout $TIME & 
 sudo /usr/xenomai/bin/./latency -s -h -p $RT_PERIOD -B 1 -H 500000 -T $TIME -g test_rt_histdata.txt | tee test_rt_kernel.log
 
-# Check if R is installed
-hash Rscript 2>/dev/null || { echo >&2 "R is needed for me to plot stats.\nYou can always do that yourself, too."; exit 0; }
-
-#echo $DISTRO $HOSTNAME $RT_KERNEL $PROCESSOR $GRAPHICS_CARD $GRAPHICS_DRIVER $RATE
-Rscript analyzeHistdata.r "$DISTRO" "$HOSTNAME" "$RT_KERNEL" "$PROCESSOR" "$GRAPHICS_CARD" "$GRAPHICS_DRIVER" "$RATE"
+Rscript makeHistPlot.r "$DISTRO" "$HOSTNAME" "$RT_KERNEL" "$PROCESSOR" "$GRAPHICS_CARD" "$GRAPHICS_DRIVER" "$RATE"
 
 exit 0
