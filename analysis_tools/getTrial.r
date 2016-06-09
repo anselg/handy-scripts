17165dcca13a316b660fb1bb3bbb92bececdf855
anselg <aesthapus@gmail.com>
2016-06-08 22:41:45 -0400

    add preliminary code - will test if r-cran-hdf5 documentation exists

--- analysis_tools/getTrial.r
+++ analysis_tools/getTrial.r
@@ -4,4 +4,18 @@
 # requires r-cran-ggplot2, r-cran-plyr, r-cran-hdf5
 ################################################################################
 
+args <- commandArgs(trailingOnly=TRUE)
 
+require("ggplot2")
+require("reshape2")
+require("plyr")
+require("hdf5")
+
+hdfname <- "test.h5"
+plotname <- "test.pdf"
+
+trialnumber <- 1
+npoints <- 1000
+
+file <- h5file("test.h5", "r")
+h5close(file)
