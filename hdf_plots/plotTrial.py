ba6e9254ca5db888afbe13db70efc4463cd38c38
Ansel <aesthapus@gmail.com>
2016-09-02 04:11:01 -0400

    update plotting function to use matplotlib

--- hdf_plots/plotTrial.py
+++ hdf_plots/plotTrial.py
@@ -1,62 +1,118 @@
-#! /bin/python
+#! /usr/bin/env python
 
-# Requires h5py, seaborn
+##########################################################################
+# Import modules
+##########################################################################
 
+import sys
+import os
+import re
 import h5py as h5
 import numpy as np
 import pandas as pd
-import seaborn as sb
 from matplotlib.backends.backend_pdf import PdfPages
+import matplotlib.pyplot as plt
+import operator
+import subprocess
 
-filename = "test.h5"
-plotname = "test.pdf"
-trialnum = 1
 
+##########################################################################
+# Parse command-line input; set global parameters
+##########################################################################
+
+if (len(sys.argv) == 0):
+    print("Give me an hdf file")
+    sys.exit()
+else:
+    filename = sys.argv[1]
+    trialnum = int(sys.argv[2])
+    plotname = os.path.splitext(filename)[0] + "_trial" + str(trialnum) + ".png"
+
+plt.style.use('ggplot')
+np.random.seed(123)
+
+
+##########################################################################
+# Define methods
+##########################################################################
 
 def getNumTrials(f):
     ntrials = len(f["/"])
     print("# of Trials:\t", ntrials)
     return ntrials
 
+
 def getTrial(f, n):
     trialname = "/Trial" + str(n)
     return f[trialname]
 
+
 def getPeriod(f, n):
     return f["/Trial" + str(n) + "/Period (ns)"].value
+
 
 def getDownsamplingRate(f, n):
     return f["/Trial" + str(n) + "/Downsampling Rate"].value
 
+
 def getTrialLength(f, n):
     return f["/Trial" + str(n) + "/Trial Length (ns)"].value
 
+
+def printChannelNames(f, n):
+    meta = [item for item in f["/Trial" + str(n) + "/Synchronous Data"]]
+    headers = [item.split(" : ")[1] for item in meta[0:len(meta) - 1]]
+    idx = 0
+    for header in headers:
+        print(str(idx) + ". " + str(header))
+        idx += 1
+    return
+
+
 def getChannelFrame(f, n):
-    meta = [ item for item in f["/Trial" + str(n) + "/Synchronous Data"] ]
-    headers = [ item.split(" : ")[1] for item in meta[0:3] ]
-    data = f["/Trial" + str(n) + "/Synchronous Data/" + str(meta[len(meta)-1])]
+    meta = [item for item in f["/Trial" + str(n) + "/Synchronous Data"]]
+    raw_headers = [item.split(" : ")[1] for item in meta[0:len(meta) - 1]]
+    num_headers = [int(item.split(" ")[0]) for item in meta[0:len(meta) - 1]]
+    dict_headers = dict(zip(num_headers, raw_headers))
+    sorted_headers = sorted(dict_headers.items(), key=operator.itemgetter(0))
+    headers = [value for key, value in sorted_headers]
+    data = f["/Trial" + str(n) + "/Synchronous Data/" +
+             str(meta[len(meta) - 1])]
     frame = pd.DataFrame(data=np.vstack(data), columns=headers)
-    frame["Time (ms)"] = range(0, len(frame))
     return frame
 
-hdf = h5.File(filename, 'r')
 
-getNumTrials(hdf)
+##########################################################################
+# Process data
+##########################################################################
 
-period = getPeriod(hdf, trialnum) # in ns
+hdf = h5.File(os.path.join(os.path.dirname(__file__), filename), 'r')
+#ntrials = int(getNumTrials(hdf))
+
+#trialnum = int(raw_input("There are " +  str(ntrials) + " trial(s). Pick one and ENTER: "))
+
+# if not ( trialnum <= ntrials and trialnum > 0):
+#    print("Invalid input\n")
+#    sys.exit()
+
+#plotname = os.path.splitext(filename)[0] + "_trial" + str(trialnum) + ".png"
+
+period = getPeriod(hdf, trialnum) / 1e6  # ns -> ms
+frequency = 1 / period  # kHz
 downsampling = getDownsamplingRate(hdf, trialnum)
-length = getTrialLength(hdf, trialnum) # in ns
+
 frame = getChannelFrame(hdf, trialnum)
-meltyframe = pd.melt(frame, "Time (ms)")
+frame["Time (s)"] = np.arange(0, len(frame)) * float(period) / 1000  # ms -> s
 
 hdf.close()
 
-# gennerate plots
-with PdfPages(plotname) as pdf:
-#    p = sb.lmplot(x="Time (ms)", y="value", hue="variable", data=meltyframe, size=5, aspect=2, fit_reg=False)
-#    pdf.savefig(p.fig)
 
-    for field in meltyframe['variable'].unique():
-        p = sb.lmplot(x="Time (ms)", y=field, aspect=2, size=5, data=frame, fit_reg=False)
-        pdf.savefig(p.fig)
+##########################################################################
+# Generate plot
+##########################################################################
 
+fig = plt.figure()
+frame.plot(subplots=True, x="Time (s)")
+plt.tight_layout()
+plt.savefig(plotname, dpi=300)
+plt.close()
