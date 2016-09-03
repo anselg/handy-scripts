b619139edb6422a1604fbd3fcfdcd89faf14ad90
Ansel <aesthapus@gmail.com>
2016-09-03 02:57:55 -0400

    Change tabs to 2 spaces beofre clang-format.
    Otherwise, it will convert tabs hidden in comments to 8 spaces.

--- misc/format_code.sh
+++ misc/format_code.sh
@@ -4,4 +4,5 @@ if ! [ -f ${HOME}/.clang-format ]; then
    clang-format -style=mozilla -dump-config > .clang-format
 fi
 
+find . -type f \( -name '*.h' -or -name '*.hpp' -or -name '*.c' -or -name '*.cpp' \) -exec sed -i "s/\t/  /g" {} +
 find . -type f \( -name '*.h' -or -name '*.hpp' -or -name '*.c' -or -name '*.cpp' \) -exec clang-format -i {} +
