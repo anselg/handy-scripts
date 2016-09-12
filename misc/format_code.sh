5308044d7e1e8781e0c1cb008647384629ef7e91
anselg <aesthapus@gmail.com>
2016-09-12 03:06:32 -0400

    use latest format

--- misc/format_code.sh
+++ misc/format_code.sh
@@ -1,9 +1,25 @@
 #! /bin/bash
+set -e
 
-if ! [ -f ${HOME}/.clang-format ]; then 
-   clang-format -style=mozilla -dump-config > .clang-format
+# get clang-format version
+CLANG_FORMAT=""
+if type -t "clang-format" > /dev/null; then 
+  CLANG_FORMAT="clang-format"
+elif type -t "clang-format-3.8" > /dev/null; then
+  CLANG_FORMAT="clang-format-3.8"
+else 
+  exit 1
 fi
 
-find . -type f \( -name '*.h' -or -name '*.hpp' -or -name '*.c' -or -name '*.cpp' \) -exec sed -i "s/\/\/\t*\(.*\)$/\/\/\1/g" {} +
-find . -type f \( -name '*.h' -or -name '*.hpp' -or -name '*.c' -or -name '*.cpp' \) -exec sed -i "s/\t/  /g" {} +
-find . -type f \( -name '*.h' -or -name '*.hpp' -or -name '*.c' -or -name '*.cpp' \) -exec clang-format -i {} +
+# generate global clang config file
+if ! [ -f ${HOME}/.clang-format ]; then 
+  ${CLANG_FORMAT} -style=mozilla -dump-config > ${HOME}/.clang-format
+fi
+
+
+find . -type f \( -name '*.h' -or -name '*.hpp' -or -name '*.c' -or -name '*.cpp' \) \
+       -exec sed -i "s/\/\/\t*\(.*\)$/\/\/\1/g" {} +
+find . -type f \( -name '*.h' -or -name '*.hpp' -or -name '*.c' -or -name '*.cpp' \) \
+       -exec sed -i "s/\t/  /g" {} +
+find . -type f \( -name '*.h' -or -name '*.hpp' -or -name '*.c' -or -name '*.cpp' \) \
+       -exec ${CLANG_FORMAT} -i {} +
