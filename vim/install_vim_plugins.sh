0aac1e9d11e4be6fe62830f2cf977b6fc3d5c9ce
anselg <aesthapus@gmail.com>
2016-09-29 19:50:13 -0400

    add vim-taglist

--- vim/install_vim_plugins.sh
+++ vim/install_vim_plugins.sh
@@ -6,12 +6,13 @@ sudo apt-get update && sudo apt-get install -y \
   vim vim-ctrlp vim-fugitive vim-latexsuite vim-pathogen vim-scripts \
   vim-ultisnips vim-youcompleteme clang-format-3.8 colordiff vim-conque cream \
   powerline fonts-powerline vim-puppet vim-python-jedi git global doxygen \
-  vim-snippets vim-syntastic
+  vim-snippets vim-syntastic exuberant-ctags
 
 # Note: vim-addons relied on system ruby, so if using rvm, be sure to use it.
 vim-addons install \
   align alternate ctrlp detectindent doxygen-toolkit fugitive nerd-commenter \
-  pathogen powerline surround syntastic tabular latex-suite youcompleteme
+  pathogen powerline surround syntastic tabular latex-suite youcompleteme \
+  taglist
 
 # Copy over the vimrc here to your home directory.
 [ -f ~/.vimrc ] && mv ~/.vimrc ~/.vimrc-bak
